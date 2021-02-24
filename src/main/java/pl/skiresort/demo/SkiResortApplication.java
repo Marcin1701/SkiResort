74cf3db1249aa35afaf924640f1487ede4901192
Marcin1701 <marcin.rogoz1@outlook.com>
2021-02-24 23:36:35 +0100

    1. Initialization commit

---
+++ src/main/java/pl/skiresort/demo/SkiResortApplication.java
@@ -0,0 +1,13 @@
+package pl.skiresort.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkiResortApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkiResortApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/skiresort/demo/SkiResortApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.skiresort.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkiResortApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
